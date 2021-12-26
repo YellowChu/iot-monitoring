4e0413e03a9eb1a96e4d06045bd9537da85827bb
David Nguyen <nguyen.david1996@gmail.com>
2021-12-26 11:58:00 +0100

    main menu

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,95 +1,72 @@
 <template>
-    <transition name="slide">
-        <div v-if="!picked" class="landing-page-nav">
-            <router-link to="/home_sensor" class="left-nav" @click="picked = true">
-                <div class="left-container">
-                    <img class="icon" src="@/assets/sensor-icon.svg">
-                    <h1 class="icon_text">Home Sensor</h1>
-                </div>      
-            </router-link>
-            <router-link to="/mailbox_notifier" class="right-nav" @click="picked = true">
-                <div class="right-container">
-                    <img class="icon" src="@/assets/mailbox-icon.svg">
-                    <h1>Mailbox Notifier</h1>
-                </div>
-            </router-link>
-        </div>
-    </transition>
-
-    <div v-if="picked" class="landing-page-view">
-        <button @click="picked = false">Back</button>
-        <router-view></router-view>
-    </div>
-    <div id="demo">
-        <h3>div slider</h3>
+    <div class="demo">
         <transition-group tag="div" class="div-slider" :name="back? 'slideback' : 'slide'">
-            <div v-if="currentIndex === 0" key="1">
-                <div class="card">
-                    DIV 1
-                </div>
+            <div v-if="currentIndex === 0" key="1" class="nav-bar-container">
+                    <router-link to="/home_sensor" class="left-nav" @click="next()">
+                        <div class="left-container">
+                            <img class="icon" src="@/assets/sensor-icon.svg">
+                            <h1 class="icon_text">Home Sensor</h1>
+                        </div>      
+                    </router-link>
+                    <router-link to="/mailbox_notifier" class="right-nav" @click="next()">
+                        <div class="right-container">
+                            <img class="icon" src="@/assets/mailbox-icon.svg">
+                            <h1>Mailbox Notifier</h1>
+                        </div>
+                    </router-link>
             </div>
-            <div v-if="currentIndex === 1" key="2" >
-                <div class="card">
-                    DIV 2
-                </div>
-            </div>
-            <div v-if="currentIndex === 2" key="3" >
-                <div class="card">
-                    DIV 3
+            <div v-if="currentIndex === 1" key="2" class="nav-bar-container">
+                <div class="view">
+                    <router-link to="/" @click="prev()">
+                        <button>Back</button>
+                    </router-link>
+                    <router-view></router-view>
                 </div>
             </div>
         </transition-group>
-        <button @click="prev()" >prev</button>
-        <button @click="next()">next</button>
     </div>
 </template>
 
 <style>
-.demo {
-  overflow: hidden;
+.nav-bar-container {
+    position: absolute;
+    display: flex;
+    height: inherit;
+    width: inherit;
+}
+
+.view {
+    background: #19507e;
+    width:inherit;
 }
 
 .slide-leave-active,
 .slide-enter-active {
-  transition: 1s;
+    transition: 1s;
 }
 .slide-enter-from {
-  transform: translate(100%, 0);
+    transform: translate(100%, 0);
 }
 .slide-leave-to {
-  transform: translate(-100%, 0);
+    transform: translate(-100%, 0);
 }
 
 .slideback-leave-active,
 .slideback-enter-active {
-  transition: 1s;
+    transition: 1s;
 }
 .slideback-enter-from {
-  transform: translate(-100%, 0);
+    transform: translate(-100%, 0);
 }
 .slideback-leave-to {
-  transform: translate(100%, 0);
+    transform: translate(100%, 0);
 }
 
 .div-slider{
-  overflow: hidden;
-  position: relative;
-  height: 100px;
-  width: 90%;
+    overflow: hidden;
+    height: 100%;
+    width: 100%;
 }
-
-.div-slider .card {
-  position: absolute;
-  height: 100px;
-  width: 90%;
-  background-color: #60adff;
-}
-
-
-.landing-page-view {
-    background: red;
-}
-
 
 .slid-enter-active,
 .slid-leave-active {
@@ -111,12 +88,9 @@ html, body {
     width: 13rem;
     fill: red;
 }
-.landing-page-nav { 
-    display: flex;
-    height: 50%;
-}
 
 .left-nav {
+    height: 100%;
     width: 50%;
     background: rgb(53, 34, 34);
     display: flex;
@@ -130,6 +104,7 @@ html, body {
 }
 
 .right-nav { 
+    height: 100%;
     flex-grow: 1;
     background: #2d373f;
     display: flex;
