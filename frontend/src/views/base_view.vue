20a3b1c7298e84eedea5a6a325e0d51b8c3adc71
David Nguyen <nguyen.david1996@gmail.com>
2022-01-31 17:51:48 +0100

    nav bar styling (still ugly tho)

--- frontend/src/views/base_view.vue
+++ frontend/src/views/base_view.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="room_sensor_view">
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark p-0" style="height: 5rem;">
         <a class="navbar-brand">
-            <b>IoT Gardienne</b>
+            <img class="icon me-4" src="@/assets/logo.png" style="width: 230;">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -21,27 +21,34 @@
 
         <div v-if="!user_state.is_authenticated" class="user-nav me-auto">
             <ul class="navbar-nav">
-                <li class="nav-item text-white">
-                    <router-link :to="{ name: 'user_login' }" class="nav-link"><font-awesome-icon :icon="['fas', 'sign-in-alt']"/> Login</router-link>
+                <li class="nav-item">
+                    <button type="button" class="btn btn-light btn-sm">
+                        <router-link :to="{ name: 'user_login' }" class="nav-link text-dark"><font-awesome-icon :icon="['fas', 'sign-in-alt']"/> Login</router-link>
+                    </button>
                 </li>
             </ul>
         </div>
         <div v-else class="user-nav me-auto">
-            <ul class="navbar-nav">
+            <ul class="user-nav navbar-nav">
                 <li class="nav-item text-white" style="cursor: pointer;">
-                    <a @click="logout()"><font-awesome-icon :icon="['fas', 'sign-out-alt']"/> Logout</a>
+                    <button type="button" class="btn btn-secondary" @click="logout()">
+                        <a><font-awesome-icon :icon="['fas', 'sign-out-alt']"/> Logout</a>
+                    </button>
                 </li>
             </ul>
         </div>
     </nav>
 
-    <div class="router_view_container">
+    <div class="router_view_container" style="margin-top: 7rem;">
         <router-view></router-view>
     </div>
 </div>
 </template>
 
 <style scoped>
+.navbar {
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+}
 .navbar-brand {
     margin-left: 1rem;
 }
@@ -53,11 +60,8 @@
 }
 .router-link-active {
     font-weight: bold;
-    background: grey;
-    border-radius: 0.5rem;
-}
-.router_view_container {
-    margin-top: 5rem;
+    background: rgb(206, 206, 206);
+    border-radius: 0.25rem;
 }
 
 .nav-item {
