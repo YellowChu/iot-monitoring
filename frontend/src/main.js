bb2944e3828449fc00a7d3ef88b67e2b245af67a
David Nguyen <nguyen.david1996@gmail.com>
2022-02-03 20:51:09 +0100

    move font awesome out of main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,34 +1,18 @@
+
+import axios from "axios";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import FontAwesomeIcon from "@/utilities/fontawesome_icons.js";
+
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import axios from "axios";
-
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
-
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faWrench, faCaretSquareDown, faChartLine, faUser, faLock, faSignInAlt, faSignOutAlt, faSlidersH, faDownload, faBatteryFull, faWaveSquare, faChevronDown, faChevronRight, faChevronLeft, faDigitalTachograph } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
 
 axios.defaults.baseURL = (window.location.protocol === "https:" ? "https://iot-monitored.herokuapp.com" : "http://localhost:8000")
 
-// TODO: move font awesome elsewhere
-library.add(faUser);
-library.add(faLock);
-library.add(faSignInAlt);
-library.add(faSignOutAlt);
-library.add(faSlidersH);
-library.add(faDownload);
-library.add(faBatteryFull);
-library.add(faWaveSquare);
-library.add(faChevronDown);
-library.add(faChevronLeft);
-library.add(faChevronRight);
-library.add(faDigitalTachograph);
-library.add(faChartLine);
-library.add(faCaretSquareDown);
-library.add(faWrench);
-
-createApp(App).use(router).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
+createApp(App)
+    .component("font-awesome-icon", FontAwesomeIcon)
+    .use(router)
+    .mount("#app");
---
+++ frontend/src/utilities/fontawesome_icons.js
@@ -0,0 +1,40 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+import { 
+    faWrench, 
+    faCaretSquareDown,
+    faChartLine,
+    faUser,
+    faLock,
+    faSignInAlt,
+    faSignOutAlt,
+    faSlidersH,
+    faDownload,
+    faBatteryFull,
+    faWaveSquare,
+    faChevronDown,
+    faChevronRight,
+    faChevronLeft,
+    faDigitalTachograph 
+} from "@fortawesome/free-solid-svg-icons";
+
+
+
+library.add(faUser);
+library.add(faLock);
+library.add(faSignInAlt);
+library.add(faSignOutAlt);
+library.add(faSlidersH);
+library.add(faDownload);
+library.add(faBatteryFull);
+library.add(faWaveSquare);
+library.add(faChevronDown);
+library.add(faChevronLeft);
+library.add(faChevronRight);
+library.add(faDigitalTachograph);
+library.add(faChartLine);
+library.add(faCaretSquareDown);
+library.add(faWrench);
+
+export default FontAwesomeIcon;
