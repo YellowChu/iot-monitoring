e2f3ab4ddf5a36b2b5440f8282867a2afc40cb1b
David Nguyen <nguyen.david1996@gmail.com>
2022-02-09 20:36:16 +0100

    sensor: add sensor code and prepare for mailbox notifier

--- apps/common/mqtt.py
+++ apps/common/mqtt.py
@@ -1,11 +1,13 @@
 import json
 import logging
-import paho.mqtt.client as mqtt
 import random
 import sys
+from base64 import b64decode
 from dateutil.parser import isoparse
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
+
+import paho.mqtt.client as mqtt
 
 from django.conf import settings
 
@@ -64,8 +66,14 @@ def parse_uplink_message(msg_payload):
         
         gateway.uplinks.add(uplink)
 
-    room_sensor, _ = RoomSensor.objects.get_or_create(device_id=device_id)
-    room_sensor.uplinks.add(uplink)
+    payload_hexed = b64decode(uplink_payload).hex()
+    sensor_code = payload_hexed[:4]
+    
+    if sensor_code == "0001":
+        room_sensor, _ = RoomSensor.objects.get_or_create(device_id=device_id)
+        room_sensor.uplinks.add(uplink)
+    elif sensor_code == "0002":
+        print("Mailbox")
 
     notify_about_uplink()
 
